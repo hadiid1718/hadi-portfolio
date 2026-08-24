bd77753bbbd3eef2d9e9c6b44823da972bbdd248
hadiid1718 <hadeed.hassan189@gmail.com>
2026-08-24 12:34:30 +0500

    Fix Hash thing

--- Frontend/src/components/common/Link.jsx
+++ Frontend/src/components/common/Link.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Link = ({ to, children, className = '' }) => (
-  <a href={`#${to}`} className={className}>
+  <a href={`${to}`} className={className}>
     {children}
   </a>
 );
